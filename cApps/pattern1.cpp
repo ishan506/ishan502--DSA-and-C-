72d782443217b414d404291ddd70a20040cffa0c
ishan506 <ishxn36@gmail.com>
2025-12-29 17:22:18 +0530

    second commit

--- cApps/pattern1.cpp
+++ cApps/pattern1.cpp
@@ -1,3 +1,4 @@
+//not complete extra star 
 #include<iostream>
 using namespace std;
 int main(){
@@ -7,7 +8,15 @@ int main(){
     for(int j=0;j<i+1;j++){
       cout<<"*";
    }
-   cout<<endl; 
-}
+   cout<<endl;
+  }
+   for (int i=0;i<n+1;i++){
+  for (int j=0;j<n-i+1;j++){
+      cout<<"*";
+    }
+    cout<<endl;
+  }
+
 return 0;
+
 }
