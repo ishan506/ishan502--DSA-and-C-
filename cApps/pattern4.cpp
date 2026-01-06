c00a7d0d7cb5ea26034f8b7b1363601031190fbb
ishan506 <ishxn36@gmail.com>
2026-01-06 21:21:28 +0530

    4th

--- cApps/pattern4.cpp
+++ cApps/pattern4.cpp
@@ -1,18 +1,19 @@
 #include<iostream>
 using namespace std;
 int main(){
-   int n;
-   cin>>n;
-for (int i=0;i<n;i++){
-    for(int j=0;j<2*i+1;j++){
-        if (j==0 || j==2*i){
-            cout<<"* ";
-     
+    int n;
+    cin>>n;
+   for (int i=0;i<n;i++){
+    int cond =i <=n/2 ? 2*i:2*(n-i-1);
+    for (int j=0;j<=cond;j++){
+        if (j<=cond/2){
+          cout<<j+1;  
+        }
+        else{
+            cout<<cond-j+1;
+        }
+     }
+cout<<endl;
+   }
     }
-    else{
-        cout<<j;
-    }
-}
-    cout<<endl;
-}
-}
+
--- cApps/pattern6.cpp
+++ cApps/pattern6.cpp
@@ -3,11 +3,11 @@ using namespace std;
 int main(){
    int n;
    cin>>n;
-   int c=1;
-for (int i=0;i<n;i++){
-for (int j=0;j<i+1;j++){
+    int c=1;
+  for (int i=1;i<n;i++){
+    for (int j=1;j<i+1;j++){
 cout<<c<<" ";
-c++;
+c=c*(i-j)/j;
 }
 cout<<endl;
 }
