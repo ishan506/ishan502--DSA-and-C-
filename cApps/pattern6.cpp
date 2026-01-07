871b47b64739522777874da9ce75abf2ac45161d
ishan506 <ishxn36@gmail.com>
2026-01-07 22:39:55 +0530

    5th

--- cApps/pattern6.cpp
+++ cApps/pattern6.cpp
@@ -3,8 +3,8 @@ using namespace std;
 int main(){
    int n;
    cin>>n;
-    int c=1;
-  for (int i=1;i<n;i++){
+   for (int i=1;i<n;i++){
+      int c=1;
     for (int j=1;j<i+1;j++){
 cout<<c<<" ";
 c=c*(i-j)/j;
